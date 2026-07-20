68ef1f6100cf4431c2f8aed3626816fdd1bfcc47
Arques <bhuvaneshkpb@gmail.com>
2026-07-21 00:08:14 +0530

    Add docker for nginx

--- v2/main.go
+++ v2/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	http.HandleFunc("/testEndPoint", customHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Error occured unable to start")
+		log.Fatalf("Error occured unable to started: %v", err)
 	}
 }
 
-func customHandler(w http.ResponseWriter, r *http.Request)  {
+func customHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello from backend server")
 }
