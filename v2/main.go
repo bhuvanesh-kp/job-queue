5031bcb1b4540425d7db996f638b39e71354d770
Arques <bhuvaneshkpb@gmail.com>
2026-07-22 23:46:34 +0530

    Add prometheus configuration

--- v2/main.go
+++ v2/main.go
@@ -1,18 +1,73 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// Define metrics
+var (
+	HttpRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "api_http_request_total",
+		Help: "Total number of requests processed by the API",
+	}, []string{"path", "status"})
+
+	HttpRequestErrorTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "api_http_request_error_total",
+		Help: "Total number of errors returned by the API",
+	}, []string{"path", "status"})
+)
+
+// Custom registry (without default Go metrics)
+var customRegistry = prometheus.NewRegistry()
+
+// Register metrics with custom registry
+func init() {
+	customRegistry.MustRegister(HttpRequestTotal, HttpRequestErrorTotal)
+}
+
 func main() {
-	http.HandleFunc("/testEndPoint", customHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Error occured unable to started: %v", err)
+	router := gin.Default()
+
+	// Register /metrics before middleware
+	router.GET("/metrics", PrometheusHandler())
+
+	router.Use(RequestMetricsMiddleware())
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Up and running!",
+		})
+	})
+	router.GET("/v1/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello from /v1/users",
+		})
+	})
+
+	router.Run(":8000")
+}
+
+// Custom metrics handler with custom registry
+func PrometheusHandler() gin.HandlerFunc {
+	h := promhttp.HandlerFor(customRegistry, promhttp.HandlerOpts{})
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
-func customHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello from backend server")
+// Middleware to record incoming requests metrics
+func RequestMetricsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		path := c.Request.URL.Path
+		c.Next()
+		status := c.Writer.Status()
+		if status < 400 {
+			HttpRequestTotal.WithLabelValues(path, strconv.Itoa(status)).Inc()
+		} else {
+			HttpRequestErrorTotal.WithLabelValues(path, strconv.Itoa(status)).Inc()
+		}
+	}
 }
